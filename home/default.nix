4ac9e88208cb1bb35e6c57bdb2f75f98245b27e3
Daniel Klein <othercriteria@gmail.com>
2025-01-29 15:56:13 -0500

    Factor out home/sway.nix

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,7 @@
   };
 
   imports = [
+    ./sway.nix
     ./tmux.nix
     ./zsh.nix
   ];
@@ -44,11 +45,6 @@
     tree
     unzip
     zip
-
-    grim # screenshots
-    slurp # screenshots
-    wlroots
-    wl-clipboard
 
     python312Packages.python
     python312Packages.virtualenv
@@ -124,56 +120,6 @@
         size = 16;
       };
     };
-  };
-
-  wayland.windowManager.sway = {
-    enable = true;
-
-    wrapperFeatures.gtk = true;
-
-    extraOptions = [
-      "--unsupported-gpu"
-    ];
-
-    config = rec {
-      modifier = "Mod4"; # command
-
-      terminal = "alacritty";
-
-      menu = "wofi --allow-images --allow-markup --show run";
-    };
-
-    # TODO: the display settings should be host-specific
-    extraConfig = ''
-      output DP-1 mode 3840x2160@119.998Hz
-      output DP-1 adaptive_sync on
-      output DP-1 subpixel rgb
-
-      input * xkb_options caps:escape
-
-      font pango:mono regular 16
-
-      bindsym Print       exec grim ~/screenshots/screenshot_$(date +"%Y-%m-%d_%H-%M-%S").png
-      bindsym Print+Shift exec grim -g "$(slurp)" ~/screenshots/screenshot_$(date +"%Y-%m-%d_%H-%M-%S").png
-
-      bindsym XF86AudioRaiseVolume exec 'wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+'
-      bindsym XF86AudioLowerVolume exec 'wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-'
-      bindsym XF86AudioMute exec 'wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle'
-    '';
-  };
-
-  programs.wofi = {
-    enable = true;
-    style = builtins.readFile ../assets/wofi-styles.css;
-  };
-
-  services.mako = {
-    enable = true;
-    layer = "overlay";
-    font = "monospace 16";
-    width = 450;
-    height = 200;
-    margin = "20";
   };
 
   home.pointerCursor = {
